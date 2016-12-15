68160034ba51de2bb5afaba94dd4a314258a42e0
BoricuaBoy98 <kevinfuentes98@gmail.com>
2016-12-15 14:48:36 -0500

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([5,6],{1155:function(n,o,c){n.exports=c(519)},519:function(n,o){}},[1155]);
+webpackJsonp([5,6],{1158:function(n,o,c){n.exports=c(518)},518:function(n,o){}},[1158]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
